4dd2802b261540265fe8b7e62dee0c3e4e2ddf67
Branko Vukelic <branko@brankovukelic.com>
2014-07-09 15:12:30 +0200

    Save original path in request object in the i18n plugin

--- librarian/i18n.py
+++ librarian/i18n.py
@@ -191,11 +191,13 @@ class I18NPlugin(object):
         """ Bottle plugin """
         ignored = route.config.get('no_i18n', False)
         def wrapper(*args, **kwargs):
+            request.original_path = request.environ.get('ORIGINAL_PATH',
+                                                        request.fullpath)
             if not ignored:
                 request.locale = locale = request.environ.get('LOCALE')
                 if locale not in self.locales:
                     # If no locale had been specified, redirect to default one
-                    path = request.environ.get('ORIGINA_PATH', '/')
+                    path = request.original_path
                     redirect(i18n_path(path, self.default_locale))
                 request.gettext = self.gettext_apis[locale]
             return callback(*args, **kwargs)
