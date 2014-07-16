0d34d7c58706268f8e9fc417cf9b2af3cf8aa2c0
Branko Vukelic <branko@brankovukelic.com>
2014-07-16 17:32:07 +0200

    Factor out content_id parameter

--- librarian/routes/content.py
+++ librarian/routes/content.py
@@ -20,6 +20,7 @@ from ..lib import send_file
 __all__ = ('app', 'content_list', 'content_file', 'content_index',)
 
 PREFIX = '/content'
+CONTENT_ID = '<content_id:re:[0-9a-f]{32}>'
 
 
 app = default_app()
@@ -32,7 +33,7 @@ def content_list():
     return {'metadata': archive.get_content()}
 
 
-@app.get(PREFIX + '/<content_id>/<filename>')
+@app.get(PREFIX + '/%s/<filename>' % CONTENT_ID)
 def content_file(content_id, filename):
     """ Serve file from zipball with specified id """
     zippath = downloads.get_zip_path(content_id)
@@ -48,7 +49,7 @@ def content_file(content_id, filename):
     return send_file.send_file(content, filename, size, timestamp)
 
 
-@app.get(PREFIX + '/<content_id>/')
+@app.get(PREFIX + '/%s/' % CONTENT_ID)
 def content_index(content_id):
     """ Shorthand for /<content_id>/index.html """
     return content_file(content_id, 'index.html')
