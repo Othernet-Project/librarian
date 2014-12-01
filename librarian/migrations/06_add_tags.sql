b3baeed3d22a6e4aadc354e0730251af2c0b722d
Branko Vukelic <branko@brankovukelic.com>
2014-12-01 16:37:21 +0100

    Fixed typo in migration code

--- librarian/migrations/06_add_tags.sql
+++ librarian/migrations/06_add_tags.sql
@@ -1,3 +1,5 @@
+begin transaction;
+
 create table tags
 (
     tag_id integer primary key asc,
@@ -9,8 +11,11 @@ create table taggings
     tag_id integer,
     md5 varchar,
     foreign key(md5) references zipballs(md5),
-    foreign key(tag_id) refrences tags(id)
+    foreign key(tag_id) references tags(id),
+    unique (tag_id, md5) on conflict ignore
 );
 
 -- Add a column that will hold cached tag values
 alter table zipballs add column tags varchar;
+
+commit transaction;
