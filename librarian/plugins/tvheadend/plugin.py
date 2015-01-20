1811b1ae1b5db5841be4b98f972cc49df63e282d
Branko Vukelic <branko@brankovukelic.com>
2015-01-20 19:32:40 +0100

    Removed unneeded import

--- librarian/plugins/tvheadend/plugin.py
+++ librarian/plugins/tvheadend/plugin.py
@@ -10,8 +10,6 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-from bottle import request
-
 from ...lib.i18n import lazy_gettext as _
 
 from ..dashboard import DashboardPlugin
