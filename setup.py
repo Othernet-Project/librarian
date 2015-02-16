11433f36478e01c852aba6c4ea5623609b7acf4f
Branko Vukelic <branko@brankovukelic.com>
2015-02-16 20:41:44 +0100

    Dramatically simplified way of add package data
    Conflicts:
    	MANIFEST.in
    	setup.py

--- setup.py
+++ setup.py
@@ -6,23 +6,13 @@ from setuptools import setup, find_packages
 
 import librarian
 
+SCRIPTDIR = os.path.dirname(__file__)
 # Production requirement, not needed during development
 BJ = ['bjoern==1.4.1']
 
 def read(fname):
     """ Return content of specified file """
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-def data(patterns=[]):
-    """ For each pattern, return mapping for data_files """
-    data_files = []
-    for pattern in patterns:
-        pattern = os.path.join('librarian', pattern)
-        paths = [os.path.normpath(p) for p in glob(pattern)]
-        for p in paths:
-            p = os.sep.join(p.split(os.sep)[1:])
-            data_files.append(p)
-    return data_files
+    return open(os.path.join(SCRIPTDIR, fname)).read()
 
 setup(
     name = 'librarian',
@@ -35,22 +25,7 @@ setup(
     keywords = 'outernet content archive library',
     url = 'https://github.com/Outernet-Project/librarian',
     packages=find_packages(),
-    package_data={
-        'librarian': data([
-            'static/css/*.css',
-            'static/img/*.png',
-            'static/img/*.gif',
-            'static/js/*.js',
-            'migrations/*.sql',
-            'plugins/*/views/*.tpl',
-            'plugins/*/views/*/*.tpl',
-            'plugins/*/static/*.*',
-            'keys/*',
-            'views/*.tpl',
-            'locales/**/LC_MESSAGES/*.[mp]o',
-            'librarian.ini',
-        ]),
-    },
+    include_package_data=True,
     long_description=read('README.rst'),
     install_requires = read('conf/requirements.txt').strip().split('\n') + BJ,
     classifiers=[
