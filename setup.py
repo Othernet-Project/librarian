5188e1d5e4c2eebd3ad4c570de4e5eb3e0d66e75
Branko Vukelic <branko@brankovukelic.com>
2015-03-08 14:39:03 +0100

    Integrated pytest

--- setup.py
+++ setup.py
@@ -1,18 +1,63 @@
 #!/usr/bin/env python
 
 import os
-import fnmatch
+import sys
+import platform
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 import librarian
 
 SCRIPTDIR = os.path.dirname(__file__)
-# Production requirement, not needed during development
-BJ = ['bjoern==1.4.2']
+PY3 = sys.version_info >= (3, 0, 0)
 
 def read(fname):
     """ Return content of specified file """
-    return open(os.path.join(SCRIPTDIR, fname)).read()
+    path = os.path.join(SCRIPTDIR, fname)
+    if PY3:
+        f = open(path, 'r', encoding='utf8')
+    else:
+        f = open(path, 'r')
+    content = f.read()
+    f.close()
+    return content
+
+def read_reqs(fname):
+    return read(fname).strip().split('\n')
+
+
+def in_scriptdir(path):
+    return os.path.join(SCRIPTDIR, os.path.normpath(path))
+
+
+# Production requirement, not needed during development
+if platform.system() == 'Windows':
+    PROD_DEPS = []
+else:
+    PROD_DEPS = ['bjoern==1.4.2']
+
+
+REQPATH = in_scriptdir('conf/requirements.txt')
+DREQPATH = in_scriptdir('conf/dev_requirements.txt')
+DEPS = read_reqs(REQPATH) + PROD_DEPS
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', 'Arguments for py.test')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -28,7 +73,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     long_description=read('README.rst'),
-    install_requires = read('conf/requirements.txt').strip().split('\n') + BJ,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Utilities',
@@ -37,4 +81,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
     ],
+    tests_require=read_reqs(DREQPATH),
+    install_requires=DEPS,
+    cmdclass={'test': PyTest},
 )
