5b97c51b6d9282263fee4a5407ac0a74c9600a81
Branko Vukelic <branko@brankovukelic.com>
2015-03-12 21:18:38 +0100

    Silence exception printing from gevent greenlets
    http://bit.ly/18fP1uo

--- librarian/app.py
+++ librarian/app.py
@@ -15,6 +15,10 @@ from __future__ import unicode_literals, print_function
 import gevent.monkey
 gevent.monkey.patch_all(aggressive=True)
 
+# For more details on the below see: http://bit.ly/18fP1uo
+import gevent.hub
+gevent.hub.Hub.NOT_ERROR=(Exception,)
+
 import sys
 import pprint
 import logging
