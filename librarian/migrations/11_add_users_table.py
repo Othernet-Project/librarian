709446931c10aae49d90c98a46f5d1079399bfec
Andrean Franc <andrean.franc@gmail.com>
2015-03-25 18:36:32 +0100

    Fix sqlite primary_key constraints to be actually unique

--- librarian/migrations/11_add_users_table.py
+++ librarian/migrations/11_add_users_table.py
@@ -1,10 +1,10 @@
 SQL = """
 create table users
 (
-    username varchar primary_key not null,   -- username
-    password varchar not null,               -- encrypted password
-    is_superuser boolean not null default 0, -- is admin user
-    created timestamp not null               -- user creation timestamp
+    username varchar primary_key unique not null,   -- username
+    password varchar not null,                      -- encrypted password
+    is_superuser boolean not null default 0,        -- is admin user
+    created timestamp not null                      -- user creation timestamp
 );
 """
 
--- librarian/migrations/12_add_sessions_table.py
+++ librarian/migrations/12_add_sessions_table.py
@@ -1,9 +1,9 @@
 SQL = """
 create table sessions
 (
-    session_id varchar primary_key not null,   -- session id
-    data varchar,                              -- arbitary session data
-    expires timestamp not null                 -- timestamp when session expires
+    session_id varchar primary_key unique not null,   -- session id
+    data varchar,                                     -- arbitary session data
+    expires timestamp not null                        -- timestamp when session expires
 );
 """
 
