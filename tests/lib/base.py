03aacd2ee70be331487e4cba704488ba346947eb
Andrean Franc <andrean.franc@gmail.com>
2015-03-30 14:12:34 +0200

    Fix base test transaction decorator

--- tests/lib/base.py
+++ tests/lib/base.py
@@ -5,18 +5,20 @@ from librarian.lib import squery
 from librarian.utils import migrations
 
 
-def transaction_test(func):
-    def _transaction_test(*args, **kwargs):
-        with mock.patch('bottle.request') as bottle_request:
-            conn = squery.Connection()
-            db = squery.Database(conn)
-            config = {'content.contentdir': '/tmp'}
-            migrations.migrate(db,
-                               in_pkg('migrations'),
-                               'librarian.migrations',
-                               config)
-            bottle_request.db = db
+def transaction_test(bottle_request_path):
+    def _transaction_test(func):
+        def __transaction_test(*args, **kwargs):
+            with mock.patch(bottle_request_path) as bottle_request:
+                conn = squery.Connection()
+                db = squery.Database(conn)
+                config = {'content.contentdir': '/tmp'}
+                migrations.migrate(db,
+                                   in_pkg('migrations'),
+                                   'librarian.migrations',
+                                   config)
+                bottle_request.db = db
 
-            return func(*args, **kwargs)
+                return func(*args, **kwargs)
 
+        return __transaction_test
     return _transaction_test
