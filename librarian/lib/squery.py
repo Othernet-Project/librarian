19c97b0a6068d747d2efc7e52ae3cbb680a32803
Andrean Franc <andrean.franc@gmail.com>
2015-04-14 14:50:05 +0200

    Fix double wrapping of database objects in DatabaseContainer, drop the now unnecessary db_connections attribute

--- librarian/lib/squery.py
+++ librarian/lib/squery.py
@@ -228,17 +228,11 @@ def get_databases(db_confs, debug=False):
     return DatabaseContainer(conns, debug=debug)
 
 
-def database_plugin(db_confs, debug=False):
-    # Connection objects are held in this closure in a semi-global variable
-    # (``connections``) in order to allow reconnection in cases where we need
-    # to disconnect temporarily (e.g., when perofrming rebuilds)
-    connections = get_connections(db_confs)
-
+def database_plugin(databases, debug=False):
     def plugin(callback):
         @wraps(callback)
         def wrapper(*args, **kwargs):
-            request.db = DatabaseContainer(connections, debug)
-            request.db_connections = connections
+            request.db = databases
             return callback(*args, **kwargs)
         return wrapper
     return plugin
