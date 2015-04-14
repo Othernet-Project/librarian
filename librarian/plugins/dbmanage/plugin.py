aec60b62eee60466e816b62d8a1aa442c8b809e6
Andrean Franc <andrean.franc@gmail.com>
2015-04-14 14:53:00 +0200

    Fix database rebuild to use new reconnect function instead of creating/assigning a new db after destroy

--- librarian/plugins/dbmanage/plugin.py
+++ librarian/plugins/dbmanage/plugin.py
@@ -21,7 +21,6 @@ from bottle_utils.i18n import lazy_gettext as _, i18n_url
 
 from ...core.archive import reload_data
 
-from ...lib import squery
 from ...lib.lock import global_lock, LockFailureError
 
 from ...utils import migrations, databases
@@ -83,16 +82,13 @@ def remove_dbfile():
             assert not os.path.exists(p), 'Expected db file to be gone'
 
 
-def run_migrations():
-    conn = squery.Database.connect(get_dbpath())
-    db = squery.Database(conn)
+def run_migrations(db):
     conf = request.app.config
     migrations.migrate(db,
                        MDIR,
                        'librarian.migrations.{0}'.format(DB_NAME),
                        conf)
     logging.debug("Finished running migrations")
-    return db
 
 
 def rebuild():
@@ -105,16 +101,15 @@ def rebuild():
     logging.debug('Acquiring global lock')
     with global_lock(always_release=True):
         db.commit()
-        db.connection.close()
+        db.close()
         backup(dbpath, bpath)
         remove_dbfile()
         logging.debug('Removed database')
-        db = request.db.main = run_migrations()
+        db.reconnect()
+        run_migrations(db)
         logging.debug('Prepared new database')
-        rows = reload_data(db)
-        db.conn.close()
+        rows = reload_data()
         logging.info('Restored metadata for %s pieces of content', rows)
-        request.db_connections['main'].connect()
     logging.debug('Released global lock')
     end = time.time()
     return end - start
