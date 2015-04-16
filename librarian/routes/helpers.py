b0d4c8072669b3da531e46584c81ffe3194d265b
Andrean Franc <andrean.franc@gmail.com>
2015-04-16 12:52:23 +0200

    Implement helper function to initialize an archive object

--- librarian/routes/helpers.py
+++ librarian/routes/helpers.py
@@ -12,15 +12,22 @@ from functools import wraps
 
 from bottle import request, abort
 
-from ..core import archive
 from ..core import downloads
 from ..core import metadata
+from ..core.archive import Archive
+
+
+def open_archive():
+    return Archive.setup(request.app.config['librarian.backend'],
+                         request.db.main,
+                         content_dir=request.app.config['content.contentdir'])
 
 
 def with_content(func):
     @wraps(func)
     def wrapper(content_id, **kwargs):
         conf = request.app.config
+        archive = open_archive()
         try:
             content = archive.get_single(content_id)
         except IndexError:
