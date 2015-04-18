877852c3276d49ad531ddc5e7f7f5ec4c5c64747
Branko Vukelic <branko@brankovukelic.com>
2015-04-18 21:27:14 +0200

    Added fixtures related to metadta
    These fixtures could use some cleanup.

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -9,6 +9,7 @@ file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
 import os
+import json
 import shutil
 import random
 import hashlib
@@ -51,4 +52,60 @@ def md5dirs():
         hashes.append(h)
         dirs.append(dpath)
     yield hashes, dirs, tmpdir
+    shutil.rmtree(tmpdir)
+
+
+@pytest.yield_fixture
+def dirs():
+    """
+    Create 20 numbered directories with 20 numbered subdirs each and return a
+    two-tuple consisting of a list of directories created and their base
+    directory.
+    """
+    tmpdir = tempfile.mkdtemp()
+    dirs = []
+    for i in range(10):
+        for n in range(10):
+            dpath = os.path.join(tmpdir, str(i + 1), str(n + 1))
+            os.makedirs(dpath)
+            dirs.append(dpath)
+    yield dirs, tmpdir
+    shutil.rmtree(tmpdir)
+
+
+@pytest.yield_fixture
+def metadata_dir(metadata):
+    """
+    Two tuple consisting of md5 hash and base directory. These should match a
+    directory tree constructed from the md5 hash, and the target directory
+    should contain a single valid info.json file.
+    """
+    tmpdir = tempfile.mkdtemp()
+    md5 = hashlib.md5()
+    md5.update(str(random.random()))
+    md5 = md5.hexdigest()
+    dpath = os.path.join(tmpdir, os.sep.join(pathcomps(md5)))
+    os.makedirs(dpath)
+    with open(os.path.join(dpath, 'info.json'), 'wb') as f:
+        json.dump(metadata, f)
+    yield md5, tmpdir
+    shutil.rmtree(tmpdir)
+
+
+@pytest.yield_fixture
+def bad_metadata_dir():
+    """
+    Two tuple consisting of md5 hash and base directory. These should match a
+    directory tree constructed from the md5 hash, and the target directory
+    should contain a single invalid info.json file.
+    """
+    tmpdir = tempfile.mkdtemp()
+    md5 = hashlib.md5()
+    md5.update(str(random.random()))
+    md5 = md5.hexdigest()
+    dpath = os.path.join(tmpdir, os.sep.join(pathcomps(md5)))
+    os.makedirs(dpath)
+    with open(os.path.join(dpath, 'info.json'), 'wb') as f:
+        f.write('bogus\n')
+    yield md5, tmpdir
     shutil.rmtree(tmpdir)
