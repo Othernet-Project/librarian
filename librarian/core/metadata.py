66350598656e0b635eebcd9d63bb642ef40a4430
Branko Vukelic <branko@brankovukelic.com>
2015-04-24 19:12:36 +0200

    Use dummy gettext to cause string extraction

--- librarian/core/metadata.py
+++ librarian/core/metadata.py
@@ -25,23 +25,26 @@ def default_broadcast(key, meta):
 
 RTL_LANGS = ['ar', 'he', 'ur', 'yi', 'ji', 'iw', 'fa']
 
+# FIXME: This is a dummy gettext to cause the strings to be extracted.
+_ = lambda x: x
+
 LICENSES = (
-    (None, 'Unknown license'),
-    ('CC-BY', 'Creative Commons Attribution'),
-    ('CC-BY-ND', 'Creative Commons Attribution-NoDerivs'),
-    ('CC-BY-NC', 'Creative Commons Attribution-NonCommercial'),
-    ('CC-BY-ND-NC', 'Creative Commons Attribution-NonCommercial-NoDerivs'),
-    ('CC-BY-SA', 'Creative Commons Attribution-ShareAlike'),
-    ('CC-BY-NC-SA', 'Creative Commons Attribution-NonCommercial-ShareAlike'),
-    ('GFDL', 'GNU Free Documentation License'),
-    ('OPL', 'Open Publication License'),
-    ('OCL', 'Open Content License'),
-    ('ADL', 'Against DRM License'),
-    ('FAL', 'Free Art License'),
-    ('PD', 'Public Domain'),
-    ('OF', 'Other free license'),
-    ('ARL', 'All rights reserved'),
-    ('ON', 'Other non-free license'),
+    (None, _('Unknown license')),
+    ('CC-BY', _('Creative Commons Attribution')),
+    ('CC-BY-ND', _('Creative Commons Attribution-NoDerivs')),
+    ('CC-BY-NC', _('Creative Commons Attribution-NonCommercial')),
+    ('CC-BY-ND-NC', _('Creative Commons Attribution-NonCommercial-NoDerivs')),
+    ('CC-BY-SA', _('Creative Commons Attribution-ShareAlike')),
+    ('CC-BY-NC-SA', _('Creative Commons Attribution-NonCommercial-ShareAlike')),
+    ('GFDL', _('GNU Free Documentation License')),
+    ('OPL', _('Open Publication License')),
+    ('OCL', _('Open Content License')),
+    ('ADL', _('Against DRM License')),
+    ('FAL', _('Free Art License')),
+    ('PD', _('Public Domain')),
+    ('OF', _('Other free license')),
+    ('ARL', _('All rights reserved')),
+    ('ON', _('Other non-free license')),
 )
 
 # `default` defaults to `None`
