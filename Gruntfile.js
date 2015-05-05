17a52e13de15b8f306fabf49b12411d270e4ab4e
Andrean Franc <andrean.franc@gmail.com>
2015-05-05 18:31:53 +0200

    Add build task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,4 +88,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['compass', 'uglify']);
 };
