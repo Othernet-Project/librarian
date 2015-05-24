829161953b4c9103b28f306fdad7a39c4d3d6205
Ben Ennis <ben@outernet.is>
2015-05-24 12:43:06 -0700

    Added polyfill for safari, opera, and possibly ie

--- assets/js/librarian.js
+++ assets/js/librarian.js
@@ -13,6 +13,23 @@
 
 (function (window, $) {
 
+  // Check if string.prototype.[end|start]swith()
+  if (!String.prototype.startsWith | !String.prototype.endsWith) {
+    String.prototype.startsWith = function(searchString, position) {
+      position = position || 0;
+      return this.lastIndexOf(searchString, position) === position;
+    };
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (position === undefined || position > subjectString.length) {
+          position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
+  }
+
   $.librarian = {};
 
   $.librarian.lang = {};
