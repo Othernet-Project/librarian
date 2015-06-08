18295259f7af26365249e9204ace5455847ef1ef
Andrean Franc <andrean.franc@gmail.com>
2015-06-08 12:00:23 +0200

    Drop static file support from menu items and use modules instead of packages

--- librarian/menu/__init__.py
+++ librarian/menu/__init__.py
@@ -10,7 +10,6 @@ file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 
 import os
 import sys
-import functools
 
 import bottle
 
@@ -24,32 +23,15 @@ MENU_ITEMS = []
 class MenuItem(object):
     label = ''
     group = None
-    icon = None
-    alt_icon = None
+    icon_class = None
+    alt_icon_class = None
     route = None
-    static_route = 'menuitems:{0}:static'
-    static_path = '/m/{0}/<path:path>'
     default_classes = ('navicon',)
 
     def __init__(self, app):
         mod_file = sys.modules[self.__class__.__module__].__file__
         pkg_path = os.path.dirname(os.path.abspath(mod_file))
         self.name = os.path.basename(pkg_path)
-        self._install_static_handler(app, pkg_path, self.name)
-
-    def _install_static_handler(self, app, pkg_path, name):
-        static_root = os.path.join(pkg_path, 'static')
-        if not os.path.isdir(static_root):
-            return
-
-        route_static = lambda path: bottle.static_file(path, static_root)
-        route_static.__name__ = '_route_{0}_static'.format(name)
-        app.route(self.static_path.format(name),
-                  'GET',
-                  route_static,
-                  name=self.static_route.format(name),
-                  no_i18n=True,
-                  skip=app.APP_ONLY_PLUGINS)
 
     def is_alt_icon_visible(self):
         return False
@@ -58,15 +40,13 @@ class MenuItem(object):
         return True
 
     def render(self):
-        IMAGE = functools.partial(html.tag, 'img')
-        static_route = self.static_route.format(self.name)
+        icon = html.SPAN(_class="icon")
         if self.is_alt_icon_visible():
-            icon_path = self.alt_icon
+            icon_class = self.alt_icon_class
         else:
-            icon_path = self.icon
+            icon_class = self.icon_class
 
-        icon = IMAGE(src=i18n_url(static_route, path=icon_path))
-        item_class = ' '.join(tuple(self.default_classes) + (self.name,))
+        item_class = ' '.join(tuple(self.default_classes) + (icon_class,))
         return html.link_other(icon + self.label,
                                i18n_url(self.route),
                                bottle.request.original_path,
@@ -81,8 +61,7 @@ class MenuItem(object):
 
 
 def install_menuitems(app):
-    for item_pkg_path in app.config.get('menu.items', []):
-        mod_path = '{0}.menuitem'.format(item_pkg_path)
+    for mod_path in app.config.get('menu.items', []):
         try:
             __import__(mod_path)  # attempt import from pythonpath
         except ImportError:
@@ -97,12 +76,16 @@ def install_menuitems(app):
 
 
 def menu_group(group):
+    """Return list of menu items that belong to the specified `group`"""
     for item in MENU_ITEMS:
         if item.group == group:
             yield item
 
 
 def menu_item(name, group=None):
+    """Return a single menu item that matches the given name. It looks through
+    either the whole list of menu items, or if `group` is specified, only on
+    the subset belonging to that group."""
     items = menu_group(group) if group else MENU_ITEMS
     for item in items:
         if item.name == name:
--- librarian/menu/content.py
+++ librarian/menu/content.py
@@ -1,5 +1,5 @@
 """
-menuitem.py: content menu item
+content.py: content menu item
 
 Copyright 2014-2015, Outernet Inc.
 Some rights reserved.
@@ -8,13 +8,13 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-from .. import MenuItem
+from . import MenuItem
 
 from bottle_utils.i18n import lazy_gettext as _
 
 
 class ContentMenuItem(MenuItem):
     label = _("Library")
-    icon = 'document.png'
+    icon_class = 'archive'
     route = 'content:list'
     group = 'main'
