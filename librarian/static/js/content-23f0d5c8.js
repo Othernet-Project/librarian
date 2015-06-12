483b714987feca11a35c5f32d1db5fe181d05c81
Branko Vukelic <branko@brankovukelic.com>
2015-06-12 19:38:22 +0200

    Added animated gifs and recompiled assets

--- librarian/static/js/content-23f0d5c8.js
+++ librarian/static/js/content-23f0d5c8.js
@@ -174,8 +174,8 @@
     url = contentPath + contentQuery.search();
 
     // Fetch the HTML data
-    loadLink.hide();
-    loader.show();
+    loadLink.addClass('active');
+    loadLink.text(loadLink.data('active'));
     xhr = $.get(url);
     xhr.done(insertContent);
     xhr.fail(insertFailure);
@@ -186,8 +186,8 @@
     if (res === '') { return loadEmpty(); }
     res = $(res);
     contentList.append(res);
-    loader.hide();
-    loadLink.show();
+    loadLink.removeClass('active');
+    loadLink.text(loadLink.data('normal'));
     win.trigger('listUpdate', [res]);
     loading = false;
   }
