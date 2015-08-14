71f82c451aebd9680e6aec4e5225adfdc056c7e8
Andrean Franc <andrean.franc@gmail.com>
2015-08-14 12:12:16 +0200

    Adjust migration script to use latest content_type values

--- librarian/migrations/main/19_add_content_type_cover_thumbnail.py
+++ librarian/migrations/main/19_add_content_type_cover_thumbnail.py
@@ -1,5 +1,5 @@
 SQL = """
-alter table zipballs add column content_type int not null default 1;
+alter table zipballs add column content_type int not null default 2;
 alter table zipballs add column cover varchar not null default "cover.jpg";
 alter table zipballs add column thumbnail varchar not null default "thumbnail.png";
 """
