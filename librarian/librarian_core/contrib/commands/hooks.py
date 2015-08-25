cc634b73a8985a4980280ddc28dd38ebb275c5c0
Andrean Franc <andrean.franc@gmail.com>
2015-08-25 14:48:55 +0200

    Add conf config param (though needs a better place somewhere else)

--- librarian/librarian_core/contrib/commands/hooks.py
+++ librarian/librarian_core/contrib/commands/hooks.py
@@ -1,8 +1,18 @@
+import os
+
 from .registry import CommandLineHandler
 
 
 def init_begin(supervisor):
     supervisor.exts.commands = CommandLineHandler(supervisor)
+    default_path = os.path.join(supervisor.config['root'],
+                                supervisor.DEFAULT_CONFIG_FILENAME)
+    supervisor.exts.commands.register('config',
+                                      None,
+                                      '--conf',
+                                      metavar='PATH',
+                                      default=default_path,
+                                      help='path to configuration file')
 
 
 def init_complete(supervisor):
