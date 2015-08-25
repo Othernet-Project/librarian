181dcdbee8ac4a5251b36dfe1ffae4ae19c4b200
Andrean Franc <andrean.franc@gmail.com>
2015-08-25 10:09:33 +0200

    Rename cache ext install function

--- librarian/librarian_cache/hooks.py
+++ librarian/librarian_cache/hooks.py
@@ -1,7 +1,7 @@
 from .cache import setup
 
 
-def cache_plugin(supervisor):
+def init_begin(supervisor):
     supervisor.exts.cache = setup(backend=supervisor.config['cache.backend'],
                                   timeout=supervisor.config['cache.timeout'],
                                   servers=supervisor.config['cache.servers'])
