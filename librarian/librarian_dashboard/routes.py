c47e7c21ff82a77b48c0f823771b2d80262895f6
Andrean Franc <andrean.franc@gmail.com>
2015-08-27 13:39:10 +0200

    Fix dashboard imports

--- librarian/librarian_dashboard/routes.py
+++ librarian/librarian_dashboard/routes.py
@@ -8,12 +8,13 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-from ..lib import auth
+from librarian.librarian_auth.decorators import login_required
+from librarian.librarian_core.contrib.templates.renderer import view
+
 from ..plugins import DASHBOARD as DASHBOARD_PLUGINS
-from ..utils.template import view
 
 
-@auth.login_required()
+@login_required()
 @view('dashboard')
 def dashboard():
     """ Render the dashboard """
