5ae92523152df1bc48b05e11a6b61ff1df405d24
Andrean Franc <andrean.franc@gmail.com>
2015-08-28 17:11:44 +0200

    Adjust librarian_sqlite to recent hook api change

--- librarian/librarian_sqlite/hooks.py
+++ librarian/librarian_sqlite/hooks.py
@@ -2,6 +2,13 @@ from .commands import dump_tables
 from .utils import close_databases, init_databases
 
 
+def initialize(supervisor):
+    supervisor.exts.commands.register('dump_tables',
+                                      dump_tables,
+                                      '--dump-tables',
+                                      action='store_true')
+
+
 def component_member_loaded(supervisor, member, config):
     supervisor.config.setdefault('database.sources', {})
     pkg_name = member['pkg_name']
@@ -11,12 +18,8 @@ def component_member_loaded(supervisor, member, config):
             supervisor.config['database.sources'][pkg_name] = db_names
 
 
-def initialize(supervisor):
+def init_complete(supervisor):
     supervisor.exts.databases = init_databases(supervisor.config)
-    supervisor.exts.commands.register('dump_tables',
-                                      dump_tables,
-                                      '--dump-tables',
-                                      action='store_true')
 
 
 def shutdown(supervisor):
