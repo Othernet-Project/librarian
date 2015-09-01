e5bcc2c1c26ae66dcccb322c08960afedf592c25
Andrean Franc <andrean.franc@gmail.com>
2015-09-01 12:55:33 +0200

    Change view_path param name to directory

--- librarian/librarian_core/contrib/templates/hooks.py
+++ librarian/librarian_core/contrib/templates/hooks.py
@@ -3,14 +3,14 @@ from .bottleconf import install_view_root, configure_bottle
 
 def component_member_loaded(supervisor, member, config):
     pkg_path = member['pkg_path']
-    view_path = config.pop('templates.view_path', None)
-    if view_path:
-        install_view_root(pkg_path, view_path)
+    directory = config.pop('templates.directory', None)
+    if directory:
+        install_view_root(pkg_path, directory)
 
 
 def initialize(supervisor):
     configure_bottle(supervisor)
     # add shared template root if it exists
-    view_path = supervisor.config.get('templates.view_path', None)
-    if view_path:
-        install_view_root(supervisor.config['root'], view_path)
+    directory = supervisor.config.get('templates.directory', None)
+    if directory:
+        install_view_root(supervisor.config['root'], directory)
