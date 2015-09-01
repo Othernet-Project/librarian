ecdb5d714f7e2d831fe5961e6cf5eec3017a5e5f
Andrean Franc <andrean.franc@gmail.com>
2015-09-01 18:16:51 +0200

    Add missing exports section to timer plugin

--- librarian/librarian_timer/plugins.py
+++ librarian/librarian_timer/plugins.py
@@ -1,6 +1,12 @@
 from .timer import request_timer
 
 
+EXPORTS = {
+    'total_timer_plugin': {},
+    'handler_timer_plugin': {}
+}
+
+
 def total_timer_plugin(super):
     return request_timer('Total')
 
