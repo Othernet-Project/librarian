84dbf9ab6cf74396f2e21fa6961ba185ff74dbb2
Ben Ennis <ben@outernet.is>
2015-10-13 15:03:39 -0700

    fixed bug, added JS for check query string

--- librarian/static/src/widgets/cleanup-buttons.js
+++ librarian/static/src/widgets/cleanup-buttons.js
@@ -1,11 +1,27 @@
 (function (window, $) {
+  $(document).ready(function () {
+    $('input[type=checkbox]').prop('checked', false);
+  });
 
-  $('input[type=checkbox]').prop('checked', false);
   $(".deselect").click(function() {
     $('input[type=checkbox]').prop('checked', false);
   });
   $(".select").click(function() {
     $('input[type=checkbox]').prop('checked', true);
+  });
+
+  $(".check").click(function() {
+    var md5_list = [];
+    $('input[type=checkbox]').each(function () {
+      var md5 = (this.checked ? $(this).attr('value') : "");
+      if (md5 === "") {
+        return;
+      }
+      md5_list.push(md5);
+    });
+    var base_string = location.href.replace(location.search,"");
+    var new_href = base_string + '?check=' + md5_list.join(separator=',');
+    location.href = new_href;
   });
 
 }(this, jQuery));
