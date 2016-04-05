f25d994b034e650b6058e1fb5dd190e8371e5697
Andrean Franc <andrean.franc@gmail.com>
2016-04-05 12:35:31 +0200

    Rewrite database config parsing and initialization routines

--- librarian/core/contrib/databases/helpers.py
+++ librarian/core/contrib/databases/helpers.py
@@ -12,26 +12,22 @@ import os
 
 import bottle
 
+from squery_pg.squery_pg import Database
+
+from ...exts import ext_container as exts
+
 
 POSTGRES_BACKEND = 'postgres'
 SQLITE_BACKEND = 'sqlite'
-SERVERLESS_DATABASE_BACKENDS = (SQLITE_BACKEND,)
+PATHS_CONFIGURABLE = (SQLITE_BACKEND,)
 
 
-def import_squery(conf):
-    backend = conf['database.backend']
-    if backend == SQLITE_BACKEND:
-        from squery_lite.squery import Database, DatabaseContainer
-    elif backend == POSTGRES_BACKEND:
-        from squery_pg.squery_pg import Database, DatabaseContainer
-    else:
-        raise ValueError('Unknown database backend: {}'.format(backend))
-
-    return (Database, DatabaseContainer)
+def is_path_configurable():
+    return exts.config['database.backend'] in PATHS_CONFIGURABLE
 
 
-def is_serverless(conf):
-    return conf['database.backend'] in SERVERLESS_DATABASE_BACKENDS
+def get_database_path(basepath, name):
+    return os.path.abspath(os.path.join(basepath, name + '.db'))
 
 
 def ensure_dir(path):
@@ -40,59 +36,31 @@ def ensure_dir(path):
         os.makedirs(path)
 
 
-def get_database_path(conf, name):
-    return os.path.abspath(os.path.join(conf['database.path'], name + '.db'))
+def register_database(name, pkg_name, basepath=None):
+    params = dict(host=exts.config['database.host'],
+                  port=exts.config['database.port'],
+                  database=name,
+                  user=exts.config['database.user'],
+                  password=exts.config['database.password'],
+                  debug=bottle.DEBUG)
+    if basepath:
+        params['path'] = get_database_path(basepath, name)
+    db = Database.connect(**params)
+    db.package_name = pkg_name
+    exts.databases[name] = db
 
 
-def get_database_configs(conf):
-    serverless = is_serverless(conf)
-    databases = dict()
-    for pkg_name, db_names in conf['database.sources'].items():
-        for name in db_names:
-            database = get_database_path(conf, name) if serverless else name
-            databases[name] = dict(package_name=pkg_name,
-                                   database=database)
-    return databases
-
-
-def get_databases(database_cls, container_cls, db_confs, host, port, user,
-                  password, debug=False):
-    databases = dict((name,
-                      database_cls.connect(host=host,
-                                           port=port,
-                                           database=db_config['database'],
-                                           user=user,
-                                           password=password,
-                                           debug=debug))
-                     for name, db_config in db_confs.items())
-    return container_cls(databases, debug=debug)
-
-
-def init_databases(config):
-    (database_cls, container_cls) = import_squery(config)
-    database_configs = get_database_configs(config)
-    if is_serverless(config):
-        # Make sure all necessary directories are present
-        for db_config in database_configs.values():
-            ensure_dir(os.path.dirname(db_config['database']))
-
-    databases = get_databases(database_cls,
-                              container_cls,
-                              database_configs,
-                              config['database.host'],
-                              config['database.port'],
-                              config['database.user'],
-                              config['database.password'],
-                              debug=bottle.DEBUG)
+def run_migrations():
+    path_configurable = is_path_configurable()
     # Run migrations on all databases
-    for db_name, db_config in database_configs.items():
-        migration_pkg = '{0}.migrations.{1}'.format(db_config['package_name'],
-                                                    db_name)
-        database_cls.migrate(databases[db_name], migration_pkg, config)
+    for (db_name, db) in exts.databases.items():
+        # Make sure all necessary directories are present
+        if path_configurable:
+            ensure_dir(os.path.dirname(db.path))
+        migration_pkg = '{0}.migrations.{1}'.format(db.package_name, db_name)
+        db.migrate(db, migration_pkg, exts.config)
 
-    return databases
 
-
-def close_databases(databases):
-    for db in databases.values():
+def close_databases():
+    for db in exts.databases.values():
         db.close()
--- librarian/core/contrib/databases/hooks.py
+++ librarian/core/contrib/databases/hooks.py
@@ -1,14 +1,16 @@
+from squery_pg.squery_pg import DatabaseContainer
+
 from .commands import dump_tables
-from .helpers import close_databases, init_databases
+from .helpers import close_databases, register_database, run_migrations
 
 
 EXPORTS = {
     'initialize': {
-        'depends_on': ['librarian_core.contrib.commands.hooks.initialize']
+        'depends_on': ['librarian.core.contrib.commands.hooks.initialize']
     },
     'component_member_loaded': {},
     'init_complete': {
-        'required_by': ['librarian_core.contrib.commands.hooks.init_complete']
+        'required_by': ['librarian.core.contrib.commands.hooks.init_complete']
     },
     'shutdown': {},
     'immediate_shutdown': {}
@@ -16,6 +18,7 @@ EXPORTS = {
 
 
 def initialize(supervisor):
+    supervisor.exts.databases = DatabaseContainer()
     supervisor.exts.commands.register('dump_tables',
                                       dump_tables,
                                       '--dump-tables',
@@ -23,21 +26,17 @@ def initialize(supervisor):
 
 
 def component_member_loaded(supervisor, member, config):
-    supervisor.config.setdefault('database.sources', {})
-    pkg_name = member['pkg_name']
-    if pkg_name not in supervisor.config['database.sources']:
-        db_names = config.pop('database.names', None)
-        if db_names:
-            supervisor.config['database.sources'][pkg_name] = db_names
+    for dbname in config.get('databases', []):
+        register_database(dbname, member['pkg_name'])
 
 
 def init_complete(supervisor):
-    supervisor.exts.databases = init_databases(supervisor.config)
+    run_migrations()
 
 
 def shutdown(supervisor):
-    close_databases(supervisor.exts.databases)
+    close_databases()
 
 
 def immediate_shutdown(supervisor):
-    close_databases(supervisor.exts.databases)
+    close_databases()
