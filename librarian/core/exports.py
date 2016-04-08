82ed2688cd747ae983a0d17593017e663aa54334
Branko Vukelic <branko@outernet.is>
2016-04-08 23:10:42 +0200

    Added member group for databases
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/exports.py
+++ librarian/core/exports.py
@@ -490,3 +490,48 @@ def Commands(MemberList):
         for name, handler in self.handlers:
             if name in arglist:
                 handler(args)
+
+
+class Databases(MemberList):
+    """
+    This class handles database members.
+    """
+    type = 'databases'
+
+    def __init__(self, supervisor):
+        super(Databases, self).__init__(supervisor)
+        exts = supervisor.exts
+        self.databases = exts.databases = DatabaseContainer({})
+        self.host = exts.config['database.host']
+        self.port = exts.config['database.port']
+        self.user = exts.config['database.user']
+        self.password = exts.config['database.password']
+        self.debug = True  # FIXME: get the value from a sane location
+
+    def get_connection(self, dbname):
+        """
+        Get a connection object for a given database.
+        """
+        return Database.connect(dataase=dbname, host=self.host, port=self.port,
+                                user=self.user, password=self.password,
+                                debug=self.debug)
+
+    def collect(self, component):
+        migrations = component.get_export('migrations', default='migrations')
+        databases = component.get_export('databases', default=[])
+        for dbname in databases:
+            migration_pkg = '{}.{}.{}'.format(component.name, migrations,
+                                              dbname)
+            self.register((dbname, migration_pkg, component.name))
+            logging.debug('Registered database {} for {}'.format(
+                dbname, component.name))
+
+    def install_member(self, database):
+        dbname, migrations_pkg, component_name = database
+        dbconn = self.get_connection(dbname)
+        dbconn.package_name = component_name
+        self.databases[dbname] = dbconn
+        migrate(dbconn, migrations_pkg, self.supervisor.config)
+        self.event.publish(DATABASE_READY, name=dbname, db=dbconn)
+        logging.info('Database {} installed for {}'.format(
+            dbname, component_name))
