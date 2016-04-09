5c844782dd8c697b8854ade5e2810f1c3a0c473e
Branko Vukelic <branko@outernet.is>
2016-04-09 20:16:38 +0200

    WIP: Plugin collector
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/collectors/plugins.py
+++ librarian/core/collectors/plugins.py
@@ -1,5 +1,21 @@
+import logging
+
 from ..exports import DependencyCollector
 
 
 class Plugins(DependencyCollector):
-    pass
+    """
+    This collector collects Bottle plugins.
+    """
+    def collect(self, component):
+        plugins = component.get_export('plugins', [])
+        for p in plugins:
+            try:
+                plugin = component.get_object(p)
+            except ImportError:
+                logging.exception('Failed to import plugin {}'.format(p))
+                continue
+            self.register(plugin)
+
+    def install_member(self, plugin):
+        self.supervisor.app.install(plugin)
