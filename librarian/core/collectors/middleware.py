c3de4bb03f8f11e48658f2d6042c5dbaf4b9ba75
Branko Vukelic <branko@outernet.is>
2016-04-09 22:04:45 +0200

    Use ObjectCollectorMixin to simplify collectors
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/collectors/middleware.py
+++ librarian/core/collectors/middleware.py
@@ -1,9 +1,7 @@
-import logging
-
-from ..exports import DependencyCollector
+from ..exports import ObjectCollectorMixin, DependencyCollector
 
 
-class Middleware(DependencyCollector):
+class Middleware(ObjectCollectorMixin, DependencyCollector):
     """
     This collector collects WSGI middleware. WSGI middleware are installed in
     reverse order such that the last-installed (first-registered) middleware
@@ -11,16 +9,6 @@ class Middleware(DependencyCollector):
     stack.
     """
     reverse_order = True
-
-    def collect(self, component):
-        middleware = component.get_export('middleware', [])
-        for m in middleware:
-            try:
-                mwcls = component.get_object(m)
-            except ImportError:
-                logging.exception('Failed to import middleware {}'.format(m))
-                continue
-            self.register(mwcls)
 
     def install_member(self, middleware):
         self.supervisor.wsgi = middleware(self.supervisor.wsgi)
--- librarian/core/collectors/plugins.py
+++ librarian/core/collectors/plugins.py
@@ -1,25 +1,13 @@
-import logging
-
-from ..exports import DependencyCollector
+from ..exports import ObjectCollectorMixin, DependencyCollector
 
 
-class Plugins(DependencyCollector):
+class Plugins(ObjectCollectorMixin, DependencyCollector):
     """
     This collector collects Bottle plugins. All plugins are installed in
     reverse order. This ensures that the last-installed plugin is invoked first
     when handling requests.
     """
     reverse_order = True
-
-    def collect(self, component):
-        plugins = component.get_export('plugins', [])
-        for p in plugins:
-            try:
-                plugin = component.get_object(p)
-            except ImportError:
-                logging.exception('Failed to import plugin {}'.format(p))
-                continue
-            self.register(plugin)
 
     def install_member(self, plugin):
         self.supervisor.app.install(plugin)
