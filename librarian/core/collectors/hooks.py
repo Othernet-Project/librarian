8f4143bb469ed69341c2934639258a19526142fc
Branko Vukelic <branko@outernet.is>
2016-04-09 19:59:36 +0200

    WIP: Added hook collector
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/collectors/hooks.py
+++ librarian/core/collectors/hooks.py
@@ -1,5 +1,27 @@
+import logging
+
 from ..exports import ListCollector
 
 
 class Hooks(ListCollector):
-    pass
+    """
+    This class collects event hooks that should be installed during supervisor
+    start-up.
+    """
+    def collect(self, component):
+        hooks = component.get_export('hooks', [])
+        for h in hooks:
+            try:
+                hook = component.get_object(h)
+            except ImportError:
+                logging.exception('Failed to import hook {}'.format(h))
+                continue
+            hook.component = component.name
+            self.register(hook)
+
+    def install_member(self, hook):
+        hook_name = getattr(hook, 'hook', None)
+        if not hook_name:
+            logging.error('Missing hook name for {}'.format(hook))
+            return
+        self.events.subscribe(hook_name, hook)
