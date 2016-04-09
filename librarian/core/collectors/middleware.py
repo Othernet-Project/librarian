20d6e4725c993e4d7ffaf2ddca614b117dc9bc9a
Branko Vukelic <branko@outernet.is>
2016-04-09 20:34:48 +0200

    WIP: Added WSGI middleware collector
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/collectors/middleware.py
+++ librarian/core/collectors/middleware.py
@@ -1,5 +1,26 @@
+import logging
+
 from ..exports import DependencyCollector
 
 
 class Middleware(DependencyCollector):
-    pass
+    """
+    This collector collects WSGI middleware. WSGI middleware are installed in
+    reverse order such that the last-installed (first-registered) middleware
+    becomes the outermost (first to handle the request) in the middleware
+    stack.
+    """
+    reverse_order = True
+
+    def collect(self, component):
+        middleware = component.get_export('middleware', [])
+        for m in middleware:
+            try:
+                mwcls = component.get_object(m)
+            except ImportError:
+                logging.exception('Failed to import middleware {}'.format(m))
+                continue
+            self.register(mwcls)
+
+    def install_member(self, middleware):
+        self.supervisor.wsgi = middleware(self.supervisor.wsgi)
