75c8d795ac72b2c544a49aa81a35203520a5dc6a
Branko Vukelic <branko@outernet.is>
2016-04-09 14:49:53 +0200

    Added mutable iterator class
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/utils.py
+++ librarian/core/utils.py
@@ -1,1 +1,40 @@
+from collections import deque
+
 from .contrib.databases.utils import *  # NOQA
+
+
+class muter(object):
+    """
+    Iterator that can be mutated while being iterated over.
+    """
+    def __init__(self, iter=[]):
+        self._iter = deque(iter)
+        self._past = deque()
+
+    def reset(self):
+        """
+        Reset the iterator to original state.
+        """
+        self._past.extend(self._iter)
+        self._iter = self._past
+        self._past = deque()
+
+    def append(self, element):
+        """
+        Append a new element to the end of the iterator. This method can be
+        called even during iteration, and will not cause the iteration to end
+        in an exception.
+        """
+        self._iter.append(element)
+
+    def next(self):
+        try:
+            current = self._iter.popleft()
+        except IndexError:
+            raise StopIteration()
+        self._past.append(current)
+        return current
+
+    def __iter__(self):
+        while True:
+            yield self.next()
---
+++ tests/core/test_utils.py
@@ -0,0 +1,73 @@
+import pytest
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from librarian.core import utils as mod
+
+
+MOD = mod.__name__
+
+
+def test_muter_basic_iteration():
+    m = mod.muter([1, 2, 3])
+    out = []
+    for i in m:
+        out.append(i)
+    assert out == [1, 2, 3]
+
+
+def test_muter_listify():
+    m = mod.muter([1, 2, 3])
+    assert list(m) == [1, 2, 3]
+
+
+def test_muter_reset():
+    m = mod.muter([1, 2, 3])
+    l1 = list(m)
+    l2 = list(m)
+    m.reset()
+    l3 = list(m)
+    assert l1 == [1, 2, 3]
+    assert l2 == []
+    assert l3 == [1, 2, 3]
+
+
+def test_reset_during_iter():
+    m = mod.muter([1, 2, 3])
+    v1 = m.next()  # <-- we are at index 1 after this
+    m.reset()
+    v2 = m.next()  # <-- we are again at index 1 after this
+    l = list(m)  # <-- get the remainder
+    assert v1 == 1
+    assert v2 == 1
+    assert l == [2, 3]
+
+
+def test_append():
+    m = mod.muter([1, 2, 3])
+    m.append(4)
+    assert list(m) == [1, 2, 3, 4]
+
+
+def test_append_during_iter():
+    m = mod.muter([1, 2, 3])
+    m.next()  # <-- remaining elements are 2, 3
+    m.append(4)
+    assert list(m) == [2, 3, 4]
+
+
+def test_append_during_iter_for_loop():
+    append_list = iter([4, 5, 6])
+
+    def appender(obj):
+        for i in append_list:
+            obj.append(i)
+    m = mod.muter([1, 2, 3])
+    out = []
+    for i in m:
+        appender(m)
+        out.append(i)
+    assert out == [1, 2, 3, 4, 5, 6]
