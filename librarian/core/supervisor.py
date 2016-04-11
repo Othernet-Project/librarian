bd7da35acad28b27ffe215ebb436b430903f6adb
Branko Vukelic <branko@outernet.is>
2016-04-11 11:59:23 +0200

    Added some unit tets for the supervisor
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/supervisor.py
+++ librarian/core/supervisor.py
@@ -21,21 +21,46 @@ class EarlyExit(Exception):
 
 
 class Supervisor:
-    LOOP_INTERVAL = 5  # in seconds
+    #: Interval of the background loop in seconds. This is the possible
+    #: shortest time in between consecutive execution of background tasks.
+    LOOP_INTERVAL = 5
+
+    #: Default name of the application configuration file.
     DEFAULT_CONFIG_FILENAME = 'config.ini'
+
+    #: Default configuration.
     CONFIG_DEFAULTS = {
         'autojson': True,
         'catchall': True
     }
 
+    # Aliases for system events
+
+    #: Component is initializing
     INITIALIZE = 'initialize'
+
+    #: Component finished loading
     COMPONENT_MEMBER_LOADED = 'component_member_loaded'
+
+    #: All components finished loading
     INIT_COMPLETE = 'init_complete'
+
+    #: Server is about to start
     PRE_START = 'pre_start'
+
+    #: Server has started
     POST_START = 'post_start'
+
+    #: New background loop cycle
     BACKGROUND = 'background'
+
+    #: Server is about to go down
     SHUTDOWN = 'shutdown'
+
+    #: Server is about to go down in an emergency
     IMMEDIATE_SHUTDOWN = 'immediate_shutdown'
+
+    #: List of all events
     APP_HOOKS = (
         INITIALIZE,
         COMPONENT_MEMBER_LOADED,
@@ -46,6 +71,11 @@ class Supervisor:
         SHUTDOWN,
         IMMEDIATE_SHUTDOWN,
     )
+
+    #: Name of the root package that contains the supervisor
+    ROOT_PKG = __name__.split('.')[0]
+
+    #: List of core components
     CORE_COMPONENTS = (
         'assets',
         'system',
@@ -67,98 +97,154 @@ class Supervisor:
         self.app.supervisor = self
         self.exts = ext_container
         self.exts.events = PubSub()
+        self.configure(root_dir)
+        self.configure_logger()
+        self.load_components()
+        self.handle_interrupt()
+        self.running = True
+        self.finalize()
 
-        # Load core configuration
-        self._configure(root_dir)
-        configure_logger(self.config)
-
-        # Load components
-        self._load_components()
-
-        # Register interrupt handler
-        on_interrupt(self.halt)
-
-        # Set flag indicating that supervisor is running
-        self._running = True
-
-        try:
-            # Fire init-complete event. Command line handlers should be
-            # executed at this point.
-            self.exts.events.publish(self.INIT_COMPLETE, self)
-        except EarlyExit as exc:
-            # One of the command line handlers probably requested early exit
-            sys.exit(exc.exit_code)
-        except Exception:
-            logging.exception("An error occurred during `init_complete`.")
-            raise
-
-    def _load_config(self, path, strict=True):
+    def load_config(self, path, strict=True):
         path = os.path.abspath(path)
         if not strict and not os.path.exists(path):
             return ConfDict()
         return ConfDict.from_file(path, defaults=self.CONFIG_DEFAULTS)
 
-    def _merge_config(self, config):
+    def merge_config(self, config):
         # merge component config into global config, but without overwriting
         # existing data
         self.config.setdefaults(config)
 
-    def _configure(self, root_dir):
+    def configure(self, root_dir):
         default_path = os.path.join(root_dir, self.DEFAULT_CONFIG_FILENAME)
         self.config_path = get_config_path(default=default_path)
         self.config = self.app.config = self._load_config(self.config_path)
         self.config['root'] = root_dir
         self.exts.config = self.config
 
-    def _install_hook(self, name, fn, **kwargs):
-        self.exts.events.subscribe(name, fn)
+    def configure_logger(self):
+        configure_logger(self.config)
+
+    def install_hook(self, name, handler, mod_name=None, **kwargs):
+        """
+        Install an event hander for given event name. For
+        :py:attr:`~Supervisor.INITIALIZE` events, ``mod_name`` argument must
+        also be passed, and the event is fired immediately for any listeners
+        that are subscribed to it.
+
+        .. note::
+            The :py:attr:`~Supervisor.INITIALIZE` events are always fired only
+            for the specific module, not as a global event.
+
+        This method is invoked by
+        :py:meth:`Supervisor.install_component_member` method. Extra keyword
+        arguments are accepted but ignored, to remain compatible with the
+        caller's API.
+        """
+        self.exts.events.subscribe(name, handler)
         # the initialize hook must be fired immediately in the scope to
         # which it belongs only
         if name == self.INITIALIZE:
-            self.exts.events.publish(name, self, scope=kwargs['mod_name'])
+            if not mod_name:
+                raise TypeError('For INITIALIZE event, mod_name is required')
+            self.exts.events.publish(name, self, scope=mod_name)
 
-    def _install_routes(self, fn, **kwargs):
-        route_config = fn(self.config)
+    def install_routes(self, factory, **kwargs):
+        """
+        Install routes returned by a route configuration factory function. The
+        ``factory`` argument is a function that accept a single positional
+        argument which is the supervisor configuration dictionary. It is
+        expected to return an interable where each element is an iterable
+        containing the following five elements:
+
+        - route name
+        - handler callable
+        - method(s)
+        - path
+        - dict of extra keyword arguments
+
+        These elements match the arguments passed to
+        :py:meth:`bottle.Bottle.route` method.
+
+        This method is invoked by
+        :py:meth:`Supervisor.install_component_member` method. Extra keyword
+        arguments are accepted but ignored, to remain compatible with the
+        caller's API.
+        """
+        route_config = factory(self.config)
         for route in route_config:
             (name, handler, method, path, kwargs) = route
             self.app.route(path, method, handler, name=name, **kwargs)
 
-    def _install_plugin(self, fn, **kwargs):
-        plugin = fn(self)
+    def install_plugin(self, factory, **kwargs):
+        """
+        Install a plugin returned by a plugin factory function. The ``factory``
+        argument is expected to be a callable that takes the supervisor
+        instance as its only argument, and returns a plugin object compatible
+        with bottle plugin API.
+
+        More information about the plugin API can be found `in the bottle
+        documentation <http://bottlepy.org/docs/dev/plugindev.html>`_.
+
+        This method is invoked by
+        :py:meth:`Supervisor.install_component_member` method. Extra keyword
+        arguments are accepted but ignored, to remain compatible with the
+        caller's API.
+        """
+        plugin = factory(self)
         self.app.install(plugin)
 
+    #: Information for handling component installation. The top-level keys are
+    #: component member types as returned by the dependency resolver. Each type
+    #: has three subkeys:
+    #:
+    #: - ``exports``: default exports for a module
+    #: - ``is_strict``: whether exports must exist
+    #: - ``handler``: a method that handles the installation
+    #:
+    #: 'Exports' are objects that a component member makes available to the
+    #: supervisor. The nature of the objects depend on the the component member
+    #: type (e.g., exports for a hooks are hook types found in the
+    #: :py:attr:`~Supervisor.APPHOOKS.` attribute). The ``'exports'`` key
+    #: provides the defaults for members that do not specify any exports.
+    #:
+    #: When ``is_strict`` flag is set for a particular member type, all exports
+    #: must exist.
+    #:
     COMPONENT_META = {
         'hooks': {
             'exports': dict(zip(APP_HOOKS, [{}] * len(APP_HOOKS))),
             'is_strict': False,
-            'handler': _install_hook
+            'handler': install_hook
         },
         'plugins': {
             'exports': {
                 'plugin': {}
             },
             'is_strict': True,
-            'handler': _install_plugin
+            'handler': install_plugin
         },
         'routes': {
             'exports': {
                 'routes': {}
             },
             'is_strict': True,
-            'handler': _install_routes
+            'handler': install_routes
         }
     }
 
-    def _get_core_components(self):
-        """Return list of import paths for all found core components."""
-        return ['.'.join([__package__, 'contrib', name])
-                for name in self.CORE_COMPONENTS]
-
-    def _install_component_member(self, member):
-        handler = self.COMPONENT_META[member['type']]['handler']
-        config_path = os.path.join(member['pkg_path'],
-                                   self.DEFAULT_CONFIG_FILENAME)
-        config = self.config.import_from_file(config_path, as_defaults=True,
+    def install_component_member(self, member):
+        """
+        Install a component members using information provided by the
+        :py:attr:`~Supervisor.CONTENT_META` dict. The component members are
+        expected to be dict-like objects that have a ``'name'``, ``'type'``,
+        and ``'pkg_path'`` keys.
+        """
+        member_type = member['type']
+        pkg_path = member['pkg_path']
+        handler = self.COMPONENT_META[member_type]['handler']
+        member_conf = os.path.join(pkg_path, self.DEFAULT_CONFIG_FILENAME)
+        config = self.config.import_from_file(member_conf, as_defaults=True,
                                               ignore_missing=True)
         handler(self, **member)
         # notify possibly other components that a new component has been
@@ -170,28 +256,88 @@ class Supervisor:
         logging.debug("LOADED: {0}".format('::'.join([member['pkg_path'],
                                                       member['name']])))
 
-    def _load_components(self):
-        components = self.config['app.components'] or []
-        # add root package to the beginning of the list
-        pkg_name = __name__.split('.')[0]
-        components = [pkg_name] + components
-        # load default core components if core override flag was not set
-        if not self.config.get('app.core_override'):
-            core_components = self._get_core_components()
-            components = core_components + components
+    def get_core_components(self):
+        """
+        Return a list of import paths for all core components listed in the
+        :py:attr:`~Supervisor.CORE_COMPONENTS` list.
+        """
+        return ['.'.join([__package__, 'contrib', name])
+                for name in self.CORE_COMPONENTS]
 
+    def get_component_list(self):
+        """
+        Return a list of all components that should be loaded. Components are
+        listed as package names that contain them.
+
+        Root package, that is the package that contains this supervisor, will
+        always be included before any package listed in ``app.components`` list
+        in the application configuration.
+
+        Unless application configuration sets an ``app.core_override`` package,
+        the core components specified in :py:attr:`~Supervisor.CORE_COMPONENTS`
+        attribute will be loaded before all other components.
+        """
+        components = [self.ROOT_PKG]
+        components.extend(self.config.get('app.components', []))
+        if not self.config.get('app.core_override'):
+            # Configuration does not specify that core is overridden by 3rd
+            # party components, so we need to load the core first.
+            components = self.get_core_components() + components
+        return components
+
+    def load_components(self):
+        """
+        Load all components based on the list returned by
+        :py:meth:`~Supervisor._get_component_list()`. The component list is
+        wrapped in dependency resolver class to facilitate ordering of loads.
+        """
+        components = self._get_component_list()
         loader = DependencyLoader(components, self.COMPONENT_META)
         for member in loader.load():
             try:
-                self._install_component_member(member)
+                self.install_component_member(member)
             except Exception:
                 logging.exception('Component member installation failed.')
 
-    def _enter_background_loop(self):
+    def handle_interrupt(self):
+        """
+        Register INT signal handler.
+        """
+        on_interrupt(self.halt)
+
+    def enter_background_loop(self):
+        """
+        Keep emitting the :py:attr:`~Supervisor.BACKGROUND` event as long as
+        :py:attr:`~Supervisor._running` flag is ``True``. The interval in which
+        the events are emitted is roughly :py:attr:`~Supervisor.LOOP_INTERVAL`
+        seconds.
+        """
         while self._running:
             sleep(self.LOOP_INTERVAL)
             # Fire background event
             self.exts.events.publish(self.BACKGROUND, self)
+
+    def finalize(self):
+        """
+        Finalize supervisor's initialization and announce the
+        :py:attr:`~Supervisor.INIT_COMPLETE` event.
+
+        If any of the components raise an :py:exc:`EarlyExit` exception during
+        this stage, the system will exit. This exception is a normal
+        flow-control mechanism, not an error condition.
+
+        Exceptions other than :py:exc:`EarlyExit` are logged and reraised.
+        """
+        try:
+            # Fire init-complete event. Command line handlers should be
+            # executed at this point.
+            self.exts.events.publish(self.INIT_COMPLETE, self)
+        except EarlyExit as exc:
+            # One of the command line handlers probably requested early exit
+            sys.exit(exc.exit_code)
+        except Exception:
+            logging.exception("An error occurred during `init_complete`.")
+            raise
 
     def start(self):
         # Fire pre-start event right before starting the WSGI server.
@@ -202,9 +348,7 @@ class Supervisor:
         self.server.start()  # non-blocking
         assert self.server.started, 'Expected server to be running'
         logging.debug("Started server on http://%s:%s/", host, port)
-        if self.config['app.debug']:
-            print('Started server on http://%s:%s/' % (host, port))
-
+        print('Started server on http://%s:%s/' % (host, port))
         # Fire post-start event after WSGI server is started.
         self.exts.events.publish(self.POST_START, self)
         # Start background loop
