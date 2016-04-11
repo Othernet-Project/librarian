f271d2d180f81dce7e29d1991868733d59b87e60
Branko Vukelic <branko@outernet.is>
2016-04-11 15:29:02 +0200

    Do not register entire component object
    In config collector, the registration is done on the entire component instance
    instaed of just configuration even though it only needs the latter. This patch
    corrects the issue.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/collectors/configuration.py
+++ librarian/core/collectors/configuration.py
@@ -6,10 +6,10 @@ class Configuration(ListCollector):
     This class manages component-specific configuration.
     """
     def collect(self, component):
-        self.register(component)
+        self.register(component.config)
 
-    def install_member(self, component):
-        for k, v in component.config.items():
+    def install_member(self, config):
+        for k, v in config.items():
             if k.startswith('exports.'):
                 # Omit exports from master configuration
                 continue
