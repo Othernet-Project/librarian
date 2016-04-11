7d9e095b333246ec42cca1e337597bf0c7eec6bc
Branko Vukelic <branko@outernet.is>
2016-04-11 15:26:48 +0200

    Added missing export_key to routes collector
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian/core/collectors/routes.py
+++ librarian/core/collectors/routes.py
@@ -5,5 +5,7 @@ class Routes(ObjectCollectorMixin, DependencyCollector):
     """
     This collector collects route handlers.
     """
+    export_key = 'routes'
+
     def install_member(self, route):
         route.route(app=self.supervisor.app)
