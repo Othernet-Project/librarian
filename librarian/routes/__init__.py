21198c3ef3c62617d374169dabe4e07cd7c212a9
Andrean Franc <andrean.franc@gmail.com>
2016-04-14 18:36:29 +0200

    Move diskspace route to class based ones

--- librarian/routes/__init__.py
+++ librarian/routes/__init__.py
@@ -19,6 +19,8 @@ def routes(config):
     auth.PasswordReset.route('/reset-password/', app=exts.bottle_app)
     auth.EmergencyReset.route('/emergency/', app=exts.bottle_app)
     dashboard.Dashboard.route(app=exts.bottle_app)
+    diskspace.Consolidate.route(app=exts.bottle_app)
+    diskspace.ConsolidateState.route(app=exts.bottle_app)
     filemanager.List.route('/files/<path:safepath>', app=exts.bottle_app)
     filemanager.Details.route('/details/<path:safepath>', app=exts.bottle_app)
     filemanager.Direct.route('/direct/<path:safepath>', app=exts.bottle_app)
@@ -35,12 +37,6 @@ def routes(config):
     setup.Exit.route(app=exts.bottle_app)
     setup.Diag.route(app=exts.bottle_app)
     route_config = (
-        ('diskspace:show_consolidate_form', diskspace.show_consolidate_form,
-         'GET', '/diskspace/consolidate/', {}),
-        ('diskspace:consolidate', diskspace.schedule_consolidate,
-         'POST', '/diskspace/consolidate/', {}),
-        ('diskspace:consolidate_state', diskspace.consolidate_state,
-         'GET', '/diskspace/consolidate/state', {}),
         ('sys:applog', logs.send_applog,
          'GET', '/' + basename(config['logging.output']), dict(unlocked=True)),
         ('sys:syslog', logs.send_diags,
--- librarian/routes/diskspace.py
+++ librarian/routes/diskspace.py
@@ -1,164 +1,106 @@
 import logging
-import functools
 
-from bottle import request
-from bottle_utils.ajax import roca_view
 from bottle_utils.i18n import i18n_url, lazy_gettext as _
-from bottle_utils.html import hsize
+from streamline import RouteBase, XHRPartialFormRoute
 
 from ..core.contrib.templates.renderer import template
 from ..core.exts import ext_container as exts
 from ..data import storage
+from ..forms.diskspace import ConsolidateForm
+
 
 gettext = lambda x: x
 
 
-# Translators, notification displayed if files were moved to
-# external storage successfully
-CONSOLIDATE_SUCCESS = gettext('Files were successfully moved to '
-                              '{storage_name}')
+class ConsolidateState(RouteBase):
+    path = '/diskspace/consolidate/state'
 
-# Translators, notification displayed if moving files to
-# external storage failed
-CONSOLIDATE_FAILURE = gettext('Files could not be moved to '
-                              '{storage_name}')
+    def get(self):
+        return dict(active=storage.get_consoildate_status())
 
 
-# Translators, notification displayed if moving files to
-# external storage only partially succeeded
-CONSOLIDATE_PARTIAL = gettext('Some files failed to copy to '
-                              '{storage_name}')
-
-
-def consolidation_notify(message, priority):
-    exts.notifications.send(
-        message,
-        category='consolidate_storage',
-        dismissable=True,
-        group='superuser',
-        priority=priority,
-        db=exts.databases.notifications)
-
-
-def clear_notifications():
-    exts.notifications.delete_by_category('consolidate_storage',
-                                          exts.databases.notifications)
-
-
-def consolidate_ok(dest):
-    logging.info('Consolidation to %s finished', dest.id)
-    message = CONSOLIDATE_SUCCESS.format(storage_name=dest.humanized_name)
-    priority = exts.notifications.NORMAL
-    consolidation_notify(message, priority)
-
-
-def consolidate_err(dest):
-    logging.error('Consolidation to %s failed', dest.id)
-    message = CONSOLIDATE_FAILURE.format(storage_name=dest.humanized_name)
-    priority = exts.notifications.URGENT
-    consolidation_notify(message, priority)
-
-
-def consolidate_partial(dest):
-    logging.error('Consolidation %s partially succeeded', dest.id)
-    message = CONSOLIDATE_PARTIAL.format(storage_name=dest.humanized_name)
-    priority = exts.notifications.URGENT
-    consolidation_notify(message, priority)
-
-
-def with_storages(fn):
-    @functools.wraps(fn)
-    def wrapper(*args, **kwargs):
-        storages = storage.get_content_storages()
-        kwargs['storages'] = storages
-        return fn(*args, **kwargs)
-    return wrapper
-
-
-def consolidate_state():
-    return dict(active=storage.get_consoildate_status())
-
-
-@roca_view('diskspace/consolidate.tpl', 'diskspace/_consolidate_form.tpl',
-           template_func=template)
-@with_storages
-def show_consolidate_form(storages):
-    return dict(storages=storages,
-                active_storage_id=storage.get_consoildate_status())
-
-
-@roca_view('diskspace/consolidate.tpl', 'diskspace/_consolidate_form.tpl',
-           template_func=template)
-@with_storages
-def schedule_consolidate(storages):
+class Consolidate(XHRPartialFormRoute):
     """ Gets a ID from request context, gathers a list of all drives and
     moves content from all other drives to the drive with matching ID """
-    supervisor = request.app.supervisor
-    tasks = supervisor.exts.tasks
-    dest_id = request.params.storage_id
-    active_storage_id = storage.get_consoildate_status()
-
-    response_ctx = {
-        'storages': storages,
-        'target_id': dest_id,
-        'active_storage_id': active_storage_id,
+    path = '/diskspace/consolidate/'
+    template_func = template
+    template_name = 'diskspace/consolidate.tpl'
+    partial_template_name = 'diskspace/_consolidate_form.tpl'
+    form_factory = ConsolidateForm
+    messages = {
+        # Translators, message shown when the file moving operation is
+        # successfully scheduled
+        'started': gettext('Files are now being moved to {destination}. You '
+                           'will be notified when the operation is finished.'),
+        # Translators, notification displayed if files were moved to
+        # external storage successfully
+        'success': gettext('Files were successfully moved to {storage_name}'),
+        # Translators, notification displayed if moving files to
+        # external storage failed
+        'failure': gettext('Files could not be moved to {storage_name}'),
+        # Translators, notification displayed if moving files to
+        # external storage only partially succeeded
+        'partial': gettext('Some files failed to copy to {storage_name}'),
     }
 
-    if active_storage_id:
-        # There is already a task running, so we can't schedule another one
-        # Translators, error message shown when moving of files is
-        # attempted while another move task is already running.
-        response_ctx['error'] = _('A scheduled move is already running. You '
-                                  'will be notified when it finishes. Please '
-                                  'try again once the current operation is '
-                                  'finished.')
-        return response_ctx
+    def clear_notifications(self):
+        exts.notifications.delete_by_category('consolidate_storage',
+                                              exts.databases.notifications)
 
-    # Perform preflight check
-    try:
-        dest = storages.move_preflight(dest_id)
-    except storage.NotFoundError:
-        # Translators, error message shown when destination drive is removed or
-        # otherwise becomes inaccessible before files are moved to it.
-        response_ctx['error'] = _('Destination drive disappeared. Please '
-                                  'reattach the drive and retry.')
-        return response_ctx
-    except storage.NoMoveTargetError:
-        # Translators, error message shown when moving files to a storage
-        # device where no other storage devices are present other than the
-        # target device.
-        response_ctx['error'] = _('There are no other drives to move files '
-                                  'from.')
-        return response_ctx
-    except storage.NothingToMoveError:
-        # Translators, error message shown when moving files to a storage
-        # device, where no movable files are present.
-        response_ctx['error'] = _('There are no files to be moved.')
-        return response_ctx
-    except storage.CapacityError as err:
-        # Not enough space on target drive
-        response_ctx['error'] = _(
-            "Not enough free space. {size} needed.").format(
-                size=hsize(err.capacity))
-        return response_ctx
+    def consolidation_notify(self, message, priority):
+        exts.notifications.send(message,
+                                category='consolidate_storage',
+                                dismissable=True,
+                                group='superuser',
+                                priority=priority,
+                                db=exts.databases.notifications)
 
-    tasks.schedule(storages.move_content_to,
-                   args=(dest_id, clear_notifications, consolidate_ok,
-                         consolidate_err, consolidate_partial),
-                   periodic=False)
-    storage.mark_consolidate_started(dest_id)
+    def success(self, dest):
+        logging.info('Consolidation to %s finished', dest.id)
+        msg = self.messages['success'].format(storage_name=dest.humanized_name)
+        priority = exts.notifications.NORMAL
+        self.consolidation_notify(msg, priority)
 
-    message = _('Files are now being moved to {destination}. You will be '
-                'notified when the operation is finished.').format(
-                    destination=dest.humanized_name)
+    def failure(self, dest):
+        logging.error('Consolidation to %s failed', dest.id)
+        msg = self.messages['failure'].format(storage_name=dest.humanized_name)
+        priority = exts.notifications.URGENT
+        self.consolidation_notify(msg, priority)
 
-    if request.is_xhr:
-        response_ctx['message'] = message
-        return response_ctx
+    def partial(self, dest):
+        logging.error('Consolidation %s partially succeeded', dest.id)
+        msg = self.messages['partial'].format(storage_name=dest.humanized_name)
+        priority = exts.notifications.URGENT
+        self.consolidation_notify(msg, priority)
 
-    return template('ui/feedback',
-                    status='success',
-                    page_title="File consolidation scheduled",
-                    message=message,
-                    redirect_url=i18n_url('dashboard:main'),
-                    redirect_target=_("settings"))
+    def get_context(self):
+        context = super(Consolidate, self).get_context()
+        context.update(storages=storage.get_content_storages(),
+                       active_storage_id=storage.get_consoildate_status(),
+                       target_id=self.request.params.get('storage_id'))
+        return context
+
+    def form_valid(self):
+        dest_id = self.form.processed_data['storage_id']
+        dest = self.form.processed_data['dest']
+        storages = self.form.processed_data['storages']
+        args = (dest_id,
+                self.clear_notifications,
+                self.success,
+                self.failure,
+                self.partial)
+        exts.tasks.schedule(storages.move_content_to, args=args)
+        storage.mark_consolidate_started(dest_id)
+        msg = self.messages['started'].format(destination=dest.humanized_name)
+        if self.request.is_xhr:
+            return dict(message=msg)
+        # Translators, used as page title when the file moving operation
+        # is successfully started
+        page_title = _("File consolidation scheduled")
+        body = template('ui/feedback',
+                        status='success',
+                        page_title=page_title,
+                        message=msg,
+                        redirect_url=i18n_url('dashboard:dashboard'),
+                        redirect_target=_("settings"))
+        return self.HTTPResponse(body=body)
