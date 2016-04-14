98e4913aa2981058e98ae431416e5bd893e9a2fb
Andrean Franc <andrean.franc@gmail.com>
2016-04-14 20:24:03 +0200

    Make strip_wrappers a fixture

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+"""
+helpers.py: Helper functions for testing
+Copyright 2014-2015, Outernet Inc.
+Some rights reserved.
+This software is free software licensed under the terms of GPLv3. See COPYING
+file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
+"""
+
+import pytest
+
+
+class Tools:
+
+    @classmethod
+    def strip_wrappers(cls, fn):
+        """ For decorated fn, return function with stirpped decorator """
+        if not hasattr(fn, 'func_closure') or not fn.func_closure:
+            return fn
+        for f in fn.func_closure:
+            f = f.cell_contents
+            if hasattr(f, '__call__'):
+                return cls.strip_wrappers(f)
+        return fn
+
+
+@pytest.fixture
+def strip_wrappers():
+    return Tools.strip_wrappers
--- tests/routes/test_dashboard.py
+++ tests/routes/test_dashboard.py
@@ -1,13 +1,11 @@
 import mock
 
-from ..helpers import strip_wrappers
-
 import librarian.routes.dashboard as mod
 
 
 @mock.patch.object(mod, 'exts')
 @mock.patch.object(mod.Dashboard, 'request')
-def test_dashboard_get(request, exts):
+def test_dashboard_get(request, exts, strip_wrappers):
     request.no_auth = True
     route = mod.Dashboard()
     get_fn = strip_wrappers(route.get)
