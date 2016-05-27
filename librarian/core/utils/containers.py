3737db8f3341fdc75956087254ae604f367ba9cd
Andrean Franc <andrean.franc@gmail.com>
2016-05-27 16:14:23 +0200

    Add branko's IterLookup to core.utils.containers

--- librarian/core/utils/containers.py
+++ librarian/core/utils/containers.py
@@ -20,3 +20,31 @@ class AttrDict(dict):
             return self[name]
         except KeyError:
             raise AttributeError(name)
+
+
+class IterLookup(object):
+    """
+    This object wraps an iterable, and returns an item with specified
+    characteristic while also creating a lookup table of items that were
+    missed speeding up lookups for those items the next time lookup is
+    performed.
+    """
+
+    def __init__(self, iterable, key=lambda x: x):
+        """
+        The ``key`` argument can be used to specify the lookup value. This
+        argument should be a function that takes an object and returns a value
+        that will be used in lookups.
+        """
+        self.iterable = ((key(i), i) for i in iterable)
+        self.lut = {}
+
+    def get(self, val):
+        if val in self.lut:
+            return self.lut[val]
+        for v, i in self.iterable:
+            self.lut[v] = i
+            if v == val:
+                return i
+        # The object was not found
+        raise KeyError('No object for key {}'.format(val))
