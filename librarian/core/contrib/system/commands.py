8fe978cb90927bdecc9501a04b903a76b2f2f69c
Andrean Franc <andrean.franc@gmail.com>
2016-06-01 17:09:17 +0200

    Delete core.contrib.commands package and merge it's command line handlers into core.contrib.system using new style handler format

--- librarian/core/contrib/system/commands.py
+++ librarian/core/contrib/system/commands.py
@@ -1,7 +1,62 @@
+import pprint
+
+from ...exceptions import EarlyExit
+from ...exts import ext_container as exts
 from .version import get_version
 
 
-def display_version(arg, supervisor):
-    ver = get_version(supervisor.config)
-    print('v%s' % ver)
-    raise supervisor.EarlyExit()
+class DebugFlag(object):
+    name = 'debug'
+    flags = '--debug'
+    kwargs = {
+        'action': 'store_true',
+        'help': 'enable debugging'
+    }
+
+    def run(self, args):
+        exts.config['app.debug'] = args.config
+
+
+class ConfigPath(object):
+    name = 'config'
+    flags = '--conf'
+    kwargs = {
+        'metavar': 'PATH',
+        'default': 'config.ini',
+        'help': 'path to configuration file'
+    }
+
+
+class DebugConfigCommand(object):
+    name = 'debug_conf'
+    flags = '--debug-conf'
+    kwargs = {
+        'action': 'store_true',
+        'help': 'print out the configuration in use and exit'
+    }
+
+    def run(self, args):
+        if not args.debug_conf:
+            return
+
+        print('Configuration file path: %s' % exts.config['config_path'])
+        pprint.pprint(exts.config, indent=4)
+        raise EarlyExit()
+
+
+class DisplayVersionCommand(object):
+    name = 'version'
+    flags = '--version'
+    kwargs = {
+        'action': 'store_true',
+        'help': 'print out version number and exit'
+    }
+
+    def run(self, args):
+        if not args.version:
+            return
+
+        print('ver', id(exts))
+        ver = get_version(exts.config)
+        print('v{}'.format(ver))
+        raise EarlyExit()
--- librarian/core/contrib/system/hooks.py
+++
@@ -1,9 +0,0 @@
-from .commands import display_version
-
-
-def initialize(supervisor):
-    supervisor.exts.commands.register('version',
-                                      display_version,
-                                      '--version',
-                                      action='store_true',
-                                      help='print out version number and exit')
