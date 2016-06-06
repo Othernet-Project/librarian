66add05769da25162d77c7f106f6933178d3f799
Andrean Franc <andrean.franc@gmail.com>
2016-06-06 14:29:46 +0200

    Adapt databases contrib package to collectors api and drop all obsolete functionality of it, including the database plugin which is not really needed anymore and is just another function call for each request to pass through

--- librarian/core/contrib/databases/hooks.py
+++ librarian/core/contrib/databases/hooks.py
@@ -1,42 +1,12 @@
-from squery_pg.squery_pg import DatabaseContainer
-
-from .commands import dump_tables
-from .helpers import close_databases, register_database, run_migrations
+from ...exports import hook
+from .helpers import close_databases
 
 
-EXPORTS = {
-    'initialize': {
-        'depends_on': ['librarian.core.contrib.commands.hooks.initialize']
-    },
-    'component_member_loaded': {},
-    'init_complete': {
-        'required_by': ['librarian.core.contrib.commands.hooks.init_complete']
-    },
-    'shutdown': {},
-    'immediate_shutdown': {}
-}
-
-
-def initialize(supervisor):
-    supervisor.exts.databases = DatabaseContainer({})
-    supervisor.exts.commands.register('dump_tables',
-                                      dump_tables,
-                                      '--dump-tables',
-                                      action='store_true')
-
-
-def component_member_loaded(supervisor, member, config):
-    for dbname in config.get('database.names', []):
-        register_database(dbname, member['pkg_name'])
-
-
-def init_complete(supervisor):
-    run_migrations()
-
-
+@hook('shutdown')
 def shutdown(supervisor):
     close_databases()
 
 
+@hook('immediate_shutdown')
 def immediate_shutdown(supervisor):
     close_databases()
