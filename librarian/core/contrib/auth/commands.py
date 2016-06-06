a8a2e648e8410d5fed6d05bb69e73230bede23ba
Andrean Franc <andrean.franc@gmail.com>
2016-06-06 18:12:06 +0200

    Adjust  to collector's expectations

--- librarian/core/contrib/auth/commands.py
+++ librarian/core/contrib/auth/commands.py
@@ -1,5 +1,7 @@
 import getpass
 
+from ...exceptions import EarlyExit
+from ...exts import ext_container as exts
 from .users import User
 
 try:
@@ -8,24 +10,40 @@ except NameError:
     pass
 
 
-def create_superuser(arg, supervisor):
-    print("Press ctrl-c to abort")
-    try:
-        username = input('Username: ')
-        password = getpass.getpass()
-    except (KeyboardInterrupt, EOFError):
-        print("Aborted")
-        raise supervisor.EarlyExit("Aborted", exit_code=1)
+class NoAuthentication(object):
+    name = 'no_auth'
+    flags = '--no-auth'
+    kwargs = {
+        'action': 'store_true',
+        'help': 'disable authentication'
+    }
 
-    try:
-        user = User.create(username=username,
-                           password=password,
-                           is_superuser=True,
-                           db=supervisor.exts.databases.auth)
-        print("User created. The password reset token is: {}".format(
-            user.reset_token))
-    except User.InvalidUserCredentials:
-        print("Invalid user credentials, please try again.")
-        create_superuser(arg, supervisor)
 
-    raise supervisor.EarlyExit()
+class CreateSuperuserCommand(object):
+    name = 'su'
+    flags = '--su'
+    kwargs = {
+        'action': 'store_true'
+    }
+
+    def run(self, args):
+        print("Press ctrl-c to abort")
+        try:
+            username = input('Username: ')
+            password = getpass.getpass()
+        except (KeyboardInterrupt, EOFError):
+            print("Aborted")
+            raise EarlyExit("Aborted", exit_code=1)
+
+        try:
+            user = User.create(username=username,
+                               password=password,
+                               is_superuser=True,
+                               db=exts.databases.auth)
+            print("User created. The password reset token is: {}".format(
+                user.reset_token))
+        except User.InvalidUserCredentials:
+            print("Invalid user credentials, please try again.")
+            self.run(args)
+        else:
+            raise EarlyExit()
--- librarian/core/contrib/auth/plugins.py
+++ librarian/core/contrib/auth/plugins.py
@@ -1,36 +1,27 @@
 import functools
 
-from bottle import request
+from bottle import request, hook
 
+from ...exports import depends_on
 from .users import User
 
 
-EXPORTS = {
-    'user_plugin': {
-        'depends_on': [
-            'librarian.core.contrib.sessions.plugins.session_plugin'
-        ]
-    }
-}
+# Set up a hook, so handlers that raise cannot escape session-saving
+@hook('after_request')
+def store_user_in_session():
+    if hasattr(request, 'session') and hasattr(request, 'user'):
+        request.user.options.collect()
+        request.session['user'] = request.user.to_json()
 
 
-def user_plugin(supervisor):
-    # Set up a hook, so handlers that raise cannot escape session-saving
-    @supervisor.app.hook('after_request')
-    def store_user_in_session():
-        if hasattr(request, 'session') and hasattr(request, 'user'):
-            request.user.options.collect()
-            request.session['user'] = request.user.to_json()
-
-    def plugin(callback):
-        @functools.wraps(callback)
-        def wrapper(*args, **kwargs):
-            request.no_auth = supervisor.config['args'].no_auth
-            user_data = request.session.get('user', '{}')
-            request.user = User.from_json(user_data)
-            request.user.options.process()
-            return callback(*args, **kwargs)
-
-        return wrapper
-    plugin.name = 'user'
-    return plugin
+@depends_on('session_plugin')
+def user_plugin(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        request.no_auth = request.app.config['args'].no_auth
+        user_data = request.session.get('user', '{}')
+        request.user = User.from_json(user_data)
+        request.user.options.process()
+        return fn(*args, **kwargs)
+    return wrapper
+user_plugin.name = 'user_plugin'
