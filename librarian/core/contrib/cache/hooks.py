23d3d8c7117fe348a0b1590d7a4f41ec1e87d048
Andrean Franc <andrean.franc@gmail.com>
2016-06-06 18:12:45 +0200

    Adjust  to collector's expectations

--- librarian/core/contrib/cache/hooks.py
+++ librarian/core/contrib/cache/hooks.py
@@ -1,6 +1,8 @@
+from ...exports import hook
 from .helpers import setup
 
 
+@hook('initialize')
 def initialize(supervisor):
     backend = supervisor.config['cache.backend']
     supervisor.exts.cache = setup(backend, supervisor.config)
