86d053cfb4751a11dae097b0656e2b7357c50bb4
Andrean Franc <andrean.franc@gmail.com>
2016-06-06 18:07:52 +0200

    Delete obsolete registration functions and add error handler route registration to initialize hook

--- librarian/hooks.py
+++ librarian/hooks.py
@@ -1,16 +1,19 @@
 import importlib
 import logging
 
+from bottle import error
+
 from fsal.client import FSAL
 from ondd_ipc.ipc import ONDDClient
 
+from .core.exports import hook
 from .core.exts import ext_container as exts
 from .data.notifications import Notification
 from .data.setup import Setup, SetupWizard
 from .helpers.notifications import invalidate_notification_cache
-from .presentation.dashboard.registry import DashboardPluginRegistry
-from .presentation.menu.registry import MenuItemRegistry
 from .presentation.settings import SettingsManager
+from .routes import system
+from .utils.filters import safepath_filter
 
 
 def import_attr(path):
@@ -28,36 +31,12 @@ def import_attr(path):
 
 def install_extensions(supervisor):
     exts.fsal = FSAL(exts.config['fsal.socket'])
-    exts.dashboard = DashboardPluginRegistry()
-    exts.menuitems = MenuItemRegistry()
     exts.notifications = Notification
     exts.notifications.on_send(invalidate_notification_cache)
     exts.ondd = ONDDClient(exts.config['ondd.socket'])
     exts.settings = SettingsManager(supervisor)
     exts.setup = Setup(supervisor)
     exts.setup_wizard = SetupWizard(name='setup')
-
-
-def register_commands():
-    for command_path in exts.config['command.handlers']:
-        command_cls = import_attr(command_path)
-        exts.commands.register(command_cls.name,
-                               command_cls(),
-                               command_cls.option,
-                               action=command_cls.action,
-                               help=command_cls.help)
-
-
-def register_menu_items():
-    for menuitem_path in exts.config['menu.items']:
-        menuitem_cls = import_attr(menuitem_path)
-        exts.menuitems.register(menuitem_cls)
-
-
-def register_dashboard_plugins():
-    for plugin_path in exts.config['dashboard.plugins']:
-        plugin_cls = import_attr(plugin_path)
-        exts.dashboard.register(plugin_cls)
 
 
 def register_wizard_steps():
@@ -80,13 +59,16 @@ def install_tasks():
         task_cls.install()
 
 
+@hook('initialize')
 def initialize(supervisor):
     install_extensions(supervisor)
-    register_commands()
-    register_menu_items()
-    register_dashboard_plugins()
     register_wizard_steps()
     register_settings()
+    supervisor.app.router.add_filter('safepath', safepath_filter)
+    error(403)(system.error_403)
+    error(404)(system.error_404)
+    error(500)(system.error_500)
+    error(503)(system.error_503)
 
 
 def init_complete(supervisor):
