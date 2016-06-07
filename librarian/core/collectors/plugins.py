1dd4d4f1a045dffdb40b76ab5baa07535f6f567b
Andrean Franc <andrean.franc@gmail.com>
2016-06-07 10:32:19 +0200

    Do not use reverse order for plugin installation

--- librarian/core/collectors/plugins.py
+++ librarian/core/collectors/plugins.py
@@ -3,12 +3,10 @@ from ..exports import ObjectCollectorMixin, DependencyCollector
 
 class Plugins(ObjectCollectorMixin, DependencyCollector):
     """
-    This collector collects Bottle plugins. All plugins are installed in
-    reverse order. This ensures that the last-installed plugin is invoked first
-    when handling requests.
+    This collector collects Bottle plugins.
     """
     export_key = 'plugins'
-    reverse_order = True
+    reverse_order = False
 
     def install_member(self, plugin):
         self.supervisor.app.install(plugin)
