bf46dfd18273ed0a5b5101cfaf9a2cac57931a69
Andrean Franc <andrean.franc@gmail.com>
2016-06-08 10:43:48 +0200

    Subscribe to db created event with su command handler instead of running directly when invoked

--- librarian/core/contrib/auth/commands.py
+++ librarian/core/contrib/auth/commands.py
@@ -25,8 +25,17 @@ class CreateSuperuserCommand(object):
     kwargs = {
         'action': 'store_true'
     }
+    #: Name of the database used for authentication data
+    DATABASE_NAME = 'auth'
 
     def run(self, args):
+        exts.events.subscribe('exp.dbready', self.db_ready)
+
+    def db_ready(self, name, db):
+        if name == self.DATABASE_NAME:
+            self.create_user()
+
+    def create_user(self):
         print("Press ctrl-c to abort")
         try:
             username = input('Username: ')
@@ -39,11 +48,11 @@ class CreateSuperuserCommand(object):
             user = User.create(username=username,
                                password=password,
                                is_superuser=True,
-                               db=exts.databases.auth)
+                               db=exts.databases[self.DATABASE_NAME])
             print("User created. The password reset token is: {}".format(
                 user.reset_token))
         except User.InvalidUserCredentials:
             print("Invalid user credentials, please try again.")
-            self.run(args)
+            self.create_user()
         else:
             raise EarlyExit()
