cc1b7d3e9b237f0bce544d45d527f8b0aa5eacaa
Andrean Franc <andrean.franc@gmail.com>
2016-06-09 10:03:53 +0200

    Fix setup plugin to comply with collector api

--- librarian/plugins/setup.py
+++ librarian/plugins/setup.py
@@ -4,25 +4,30 @@ from bottle import request, redirect
 from bottle_utils.i18n import i18n_url
 
 from ..core.contrib.templates.renderer import template
+from ..core.exts import ext_container as exts
 
 
 NO_XHR_TEMPLATE = 'setup/no_xhr.tpl'
 
 
-def plugin(supervisor):
-    setup_path = i18n_url('setup:main')
-    ignored_paths = [setup_path, i18n_url('setup:diag')]
+def setup_plugin(fn):
+    """
+    On incoming requests, check if the setup process has been already completed
+    and in case it wasn't, redirect to the setup wizard.
 
-    def plugin(callback):
-        @functools.wraps(callback)
-        def wrapper(*args, **kwargs):
-            if (not supervisor.exts.setup_wizard.is_completed and
-                    not any([request.path == path[len(request.locale) + 1:]
-                             for path in ignored_paths])):
-                if request.is_xhr:
-                    return template(NO_XHR_TEMPLATE)
-                return redirect(setup_path)
-            return callback(*args, **kwargs)
-        return wrapper
-    plugin.name = 'setup'
-    return plugin
+    Exclusions to this rule are paths belonging to the setup wizard itself and
+    static paths.
+    """
+    setup_path = i18n_url('setup:enter')
+    excluded_paths = [setup_path, i18n_url('setup:diag')]
+    is_excluded = lambda: any(request.path == path[len(request.locale) + 1:]
+                              for path in excluded_paths)
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        if not exts.setup_wizard.is_completed and not is_excluded():
+            return (template(NO_XHR_TEMPLATE)
+                    if request.is_xhr else redirect(setup_path))
+        return fn(*args, **kwargs)
+    return wrapper
+setup_plugin.name = 'setup'
