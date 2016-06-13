636961789ccff88d075dcbc444e617f81f0152ff
Andrean Franc <andrean.franc@gmail.com>
2016-06-13 18:22:46 +0200

    Remove obsolete core.contrib.tasks and make task collector take over it's functionality completely

--- librarian/core/contrib/tasks/hooks.py
+++
@@ -1,9 +0,0 @@
-from greentasks.scheduler import TaskScheduler
-
-from ...exports import hook
-
-
-@hook('initialize')
-def initialize(supervisor):
-    delay = supervisor.config['app.consume_tasks_delay']
-    supervisor.exts.tasks = TaskScheduler(consume_tasks_delay=delay)
