b27bfbfd64bc19ceefe200e35e317ebb81c79982
Andrean Franc <andrean.franc@gmail.com>
2016-07-07 17:44:38 +0200

    Add missing template vars to step context

--- librarian/setup/ondd.py
+++ librarian/setup/ondd.py
@@ -44,8 +44,18 @@ class ONDDStep:
         ondd_client = request.app.supervisor.exts.ondd
         snr_min = request.app.config.get('ondd.snr_min', 0.2)
         snr_max = request.app.config.get('ondd.snr_max', 0.9)
-        return dict(status=ondd_client.get_status(), form=ONDDForm(),
-                    SNR_MIN=snr_min, SNR_MAX=snr_max)
+        ONDDForm = get_form()
+        band = ondd_helpers.get_band()
+        return dict(status=ondd_client.get_status(),
+                    band=band,
+                    lband=ondd_helpers.LBAND,
+                    kuband=ondd_helpers.KUBAND,
+                    is_l=band == ondd_helpers.LBAND,
+                    is_ku=band == ondd_helpers.KUBAND,
+                    preset_keys=ONDDForm.PRESETS[0].values.keys(),
+                    form=ONDDForm(),
+                    SNR_MIN=snr_min,
+                    SNR_MAX=snr_max)
 
     @staticmethod
     def post():
@@ -56,28 +66,44 @@ class ONDDStep:
         form_valid = form.is_valid()
         snr_min = request.app.config.get('ondd.snr_min', 0.2)
         snr_max = request.app.config.get('ondd.snr_max', 0.9)
+        band = ondd_helpers.get_band()
 
         if form_valid:
             # Store full settings
             logging.info('ONDD: tuner settings updated')
             data = {'ondd': form.processed_data}
             request.app.supervisor.exts.setup.append(data)
-
             if is_test_mode:
-                return dict(successful=False, form=form,
+                return dict(successful=False,
+                            form=form,
                             status=ondd_client.get_status(),
                             # Translators, shown when tuner settings are
                             # updated during setup wizard step.
                             message=_('Tuner settings have been updated'),
-                            SNR_MIN=snr_min, SNR_MAX=snr_max)
+                            band=band,
+                            lband=ondd_helpers.LBAND,
+                            kuband=ondd_helpers.KUBAND,
+                            is_l=band == ondd_helpers.LBAND,
+                            is_ku=band == ondd_helpers.KUBAND,
+                            preset_keys=ONDDForm.PRESETS[0].values.keys(),
+                            SNR_MIN=snr_min,
+                            SNR_MAX=snr_max)
             return dict(successful=True)
 
         # Form is not valid
         if is_test_mode:
             # We only do something about this in test mode
-            return dict(successful=False, form=form,
+            return dict(successful=False,
+                        form=form,
                         status=ondd_client.get_status(),
-                        SNR_MIN=snr_min, SNR_MAX=snr_max)
+                        band=band,
+                        lband=ondd_helpers.LBAND,
+                        kuband=ondd_helpers.KUBAND,
+                        is_l=band == ondd_helpers.LBAND,
+                        is_ku=band == ondd_helpers.KUBAND,
+                        preset_keys=ONDDForm.PRESETS[0].values.keys(),
+                        SNR_MIN=snr_min,
+                        SNR_MAX=snr_max)
 
         request.app.supervisor.exts.setup.append({'ondd': {}})
         return dict(successful=True)
