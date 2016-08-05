2fc79826b82499b7c87b1067395d713bc07cda52
Andrean Franc <andrean.franc@gmail.com>
2016-08-05 13:02:03 +0200

    Expose state object through template helpers

--- librarian/core/collectors/state.py
+++ librarian/core/collectors/state.py
@@ -1,3 +1,4 @@
+from ..contrib.templates.decorators import template_helper
 from ..exports import (ObjectCollectorMixin, RegistryInstallerMixin,
                        ListCollector)
 from ...data.state.container import StateContainer
@@ -11,3 +12,7 @@ class StateProviders(ObjectCollectorMixin, RegistryInstallerMixin,
     export_key = 'state'
     registry_class = StateContainer
     ext_name = 'state'
+
+    def __init__(self, *args, **kwargs):
+        super(StateProviders, self).__init__(*args, **kwargs)
+        template_helper('state')(self.regobj)
